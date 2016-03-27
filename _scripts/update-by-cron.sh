cfb816284d163083e95f80c06a257cff62d87c6e
MURAOKA Taro <koron.kaoriya@gmail.com>
2016-03-27 11:42:21 +0900

    fix bugs

--- _scripts/update-by-cron.sh
+++ _scripts/update-by-cron.sh
@@ -12,22 +12,28 @@ DIR=$1 ; shift
 USER_NAME="redirects cron updater"
 USER_EMAIL="redirects+cron%$(hostname -s)@vim-jp.org"
 
-if [ ! -d "$DIR" ] ; then
+# Setup working directory and cd to it.
+if [ -d "$DIR" ] ; then
+  cd "$DIR"
+else
   parent=$(dirname "$DIR")
-  echo $parent
   if [ ! -d "$parent" ] ; then
     mkdir -p "$parent"
   fi
-  git clone -b gh-pages --depth 50 "$REPO" "$DIR"
+  git clone -b gh-pages --depth 50 --quiet "$REPO" "$DIR"
+  cd "$DIR"
   git config push.default simple
   git config user.email "$USER_EMAIL"
   git config user.name "$USER_NAME"
 fi
 
-cd "$DIR"
+# Update repository.
 go run _scripts/vim_jp-redirects-update/main.go
 git add --update
+
+# Commit changes.
 if ! git diff --quiet HEAD ; then
   git commit -m "Updated by cron on $(hostname -s) at $(date "+%Y/%m/%d %H:%M %Z")"
+  # Disabled for debug.
   #git push --quiet
 fi
